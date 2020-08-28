83a00a6773f9005d57090e8b7c1ff7f94f4e880c
teruto725 <akiteruto@gmail.com>
2020-08-28 16:41:38 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -45,7 +46,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-
   private
 
     def user_params
@@ -53,23 +53,15 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # beforeアクション
+    # beforeフィルター
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 
-    # 管理者かどうか確認
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
